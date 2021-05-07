e8d5c0ecc14d716e24ce3d21269ab5d7e5e7211a
Dhruv Vajpeyi <vajpeyi2@gmail.com>
2021-05-07 18:33:52 -0400

    Initial side tracking

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ cap = cv2.VideoCapture(0)
 #cap2 = cv2.VideoCapture(0)
 
 tracker_fn = cv2.TrackerCSRT_create
-tracker1 = None
-tracker2 = None
+tracker1_top = None
+tracker2_top = None
+
+tracker1_side = None
+tracker2_side = None
+
 circles = None
 while True:
     # Capture frame-by-frame
@@ -18,11 +22,10 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     smoothed_gray = cv2.GaussianBlur(gray, (7, 7), 1.5)
-    smoothed_frame = cv2.GaussianBlur(frame, (7, 7), 1.5)
+    # smoothed_gray2 = cv2.GaussianBlur(gray2, (7, 7), 1.5)
 
-    # circles2 = get_circles(gray2)
-    
-    # draw_circles(circles2, gray2)
+    smoothed_frame = cv2.GaussianBlur(frame, (7, 7), 1.5)
+    # smoothed_frame2 = cv2.GaussianBlur(frame2, (7, 7), 1.5)
 
     if cv2.waitKey(1) & 0xFF == ord("w"):
         circles = get_circles(smoothed_gray)
@@ -30,16 +33,22 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord("s"):
         # select the bounding box of the object we want to track (make
         # sure you press ENTER or SPACE after selecting the ROI)
-        tracker1 = init_tracker(smoothed_frame, "Tracker 1", tracker_fn)
-        tracker2 = init_tracker(smoothed_frame, "Tracker 2", tracker_fn)
+        tracker1_top = init_tracker(smoothed_frame, "Tracker 1", tracker_fn)
+        tracker2_top = init_tracker(smoothed_frame, "Tracker 2", tracker_fn)
+        # tracker1_side = init_tracker(smoothed_frame2, "Tracker 1", tracker_fn)
+        # tracker2_side = init_tracker(smoothed_frame2, "Tracker 2", tracker_fn)
 
-    box1 = draw_bb(tracker1, smoothed_frame)
-    box2 = draw_bb(tracker2, smoothed_frame)
+
+    box1_top = draw_bb(tracker1_top, smoothed_frame)
+    box2_top = draw_bb(tracker2_top, smoothed_frame)
+
+    #box1_side = draw_bb(tracker1_side, smoothed_frame2)
+    #box2_side = draw_bb(tracker2_side, smoothed_frame2)
     
     if circles is not None:
         draw_circles(circles, smoothed_frame)
-        circle_idx1 = get_overlapped_circles(circles, box1)
-        circle_idx2 = get_overlapped_circles(circles, box2)
+        circle_idx1 = get_overlapped_circles(circles, box1_top)
+        circle_idx2 = get_overlapped_circles(circles, box2_top)
 
         if (circle_idx1 != -1):
             print("Stick 1 overlapping: " + str(circle_idx1))
