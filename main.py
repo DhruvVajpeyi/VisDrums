f20133fd5f361aa1709efb9fa2cc45e37c57e89c
Dhruv Vajpeyi <vajpeyi2@gmail.com>
2021-05-13 15:24:15 -0400

    Slight fixes, sounds chosen by boxes

--- main.py
+++ main.py
@@ -23,63 +23,62 @@ reset_hit_1 = True
 reset_hit_2 = True
 
 circles = None
+
+sounds = ["samples/bass.wav", "samples/hat.wav", "samples/snare.wav"]
 while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
     ret2, frame2 = cap2.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-    smoothed_gray = cv2.GaussianBlur(gray, (7, 7), 1.5)
-    smoothed_gray2 = cv2.GaussianBlur(gray2, (7, 7), 1.5)
-
-    smoothed_frame = cv2.GaussianBlur(frame, (7, 7), 1.5)
-    smoothed_frame2 = cv2.GaussianBlur(frame2, (7, 7), 1.5)
+    #smoothed_frame = cv2.GaussianBlur(frame, (7, 7), 1.5)
+    #smoothed_frame2 = cv2.GaussianBlur(frame2, (7, 7), 1.5)
 
     if cv2.waitKey(1) & 0xFF == ord("c"):
-        circles = get_circles(gray)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        smoothed_gray = cv2.GaussianBlur(gray, (7, 7), 1.5)
+        circles = get_circles(smoothed_gray)
 
     if cv2.waitKey(1) & 0xFF == ord("t"):
         # select the bounding box of the object we want to track (make
         # sure you press ENTER or SPACE after selecting the ROI)
-        tracker1_top = init_tracker(smoothed_gray, "Tracker 1", tracker_fn)
-        tracker2_top = init_tracker(smoothed_gray, "Tracker 2", tracker_fn)
+        tracker1_top = init_tracker(frame, "Tracker 1", tracker_fn)
+        tracker2_top = init_tracker(frame, "Tracker 2", tracker_fn)
     if cv2.waitKey(1) & 0xFF == ord("s"):
-        tracker1_side = init_tracker(smoothed_gray2, "Tracker 3", tracker_fn)
-        tracker2_side = init_tracker(smoothed_gray2, "Tracker 4", tracker_fn)
+        tracker1_side = init_tracker(frame2, "Tracker 3", tracker_fn)
+        tracker2_side = init_tracker(frame2, "Tracker 4", tracker_fn)
 
-    box1_top = draw_bb(tracker1_top, smoothed_frame)
-    box2_top = draw_bb(tracker2_top, smoothed_frame)
+    box1_top = draw_bb(tracker1_top, frame)
+    box2_top = draw_bb(tracker2_top, frame)
 
-    box1_side = draw_bb(tracker1_side, smoothed_frame2)
-    box2_side = draw_bb(tracker2_side, smoothed_frame2)
+    box1_side = draw_bb(tracker1_side, frame2)
+    box2_side = draw_bb(tracker2_side, frame2)
 
+    below_line_1 = check_if_below_line(frame2.shape[0] - line_threshold, box1_side)
+    below_line_2 = check_if_below_line(frame2.shape[0] - line_threshold, box2_side)
+    reset_hit_1 = check_if_reset_line(frame2.shape[0] - line_threshold, box1_side, reset_hit_1)
+    reset_hit_2 = check_if_reset_line(frame2.shape[0] - line_threshold, box2_side, reset_hit_2)
+    
     if circles is not None:
-        draw_circles(circles, smoothed_frame)
+        draw_circles(circles, frame)
         circle_idx1 = get_overlapped_circles(circles, box1_top)
         circle_idx2 = get_overlapped_circles(circles, box2_top)
 
         if circle_idx1 != -1 and below_line_1 and reset_hit_1:
             print("Stick 1 overlapping: " + str(circle_idx1))
-            playsound.playsound("samples/bass.wav", block=False)
+            playsound.playsound(sounds[circle_idx1], block=False)
             reset_hit_1 = False
         if circle_idx2 != -1 and below_line_2 and reset_hit_2:
             print("Stick 2 overlapping: " + str(circle_idx2))
-            playsound.playsound("samples/bass.wav", block=False)
+            playsound.playsound(sounds[circle_idx2], block=False)
             reset_hit_2 = False
 
-    line = cv2.line(smoothed_frame2, (0, smoothed_frame2.shape[0] - line_threshold),
-                    (smoothed_frame2.shape[1], smoothed_frame2.shape[0] - line_threshold), (0, 0, 255),
-                    7)
-    below_line_1 = check_if_below_line(smoothed_frame2.shape[0] - line_threshold, box1_side)
-    below_line_2 = check_if_below_line(smoothed_frame2.shape[0] - line_threshold, box2_side)
-    reset_hit_1 = check_if_reset_line(smoothed_frame2.shape[0] - line_threshold, box1_side, reset_hit_1)
-    reset_hit_2 = check_if_reset_line(smoothed_frame2.shape[0] - line_threshold, box2_side, reset_hit_2)
 
+    line = cv2.line(frame2, (0, frame2.shape[0] - line_threshold),
+                    (frame2.shape[1], frame2.shape[0] - line_threshold), (0, 0, 255),
+                    7)
     # Display the resulting frame
-    cv2.imshow('frame', smoothed_frame)
-    cv2.imshow('frame2', smoothed_frame2)
+    cv2.imshow('frame', frame)
+    cv2.imshow('frame2', frame2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
